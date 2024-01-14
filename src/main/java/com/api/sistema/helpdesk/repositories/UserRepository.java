18e6a9e7be57a4b4df2252b5118fa6d65785a962
Hitalo Siriano <hitalo.siriano@gmail.com>
2024-01-14 11:16:27 -0300

    corrigindo erro

--- src/main/java/com/api/sistema/helpdesk/repositories/UserRepository.java
+++ src/main/java/com/api/sistema/helpdesk/repositories/UserRepository.java
@@ -4,13 +4,14 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.api.sistema.helpdesk.models.UserModel;
 import java.util.List;
 
 
-
+@EnableJpaRepositories
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
 
    UserDetails findByLogin(String login);
